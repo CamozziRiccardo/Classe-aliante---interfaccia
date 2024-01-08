0819355c936e519a68facad587f253c2db532043
Riccardo Camozzi <erdodopazzosgravo@gmail.com>
2024-01-08 16:27:11 +0100

    Aggiunta file Coda e corrispondente Classe

---
+++ Classe aliante - interfaccia/Coda.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Coda : Componente
+{
+    private double lunghezza;
+
+    public Coda(double lunghezza)
+    {
+        this.lunghezza = lunghezza;
+    }
+
+    public string Descrizione()
+    {
+        return $"Coda - Lunghezza: {lunghezza}";
+    }
+
+    public double Costo()
+    {
+        // Calcolo del costo della coda basato sulla lunghezza
+        // Questo è un valore fittizio, sostituiscilo con un calcolo reale
+        return lunghezza * 50;
+    }
+}
