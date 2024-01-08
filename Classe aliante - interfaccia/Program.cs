1f28b85dddc6b37723a975f8fab40fd57c8ce6ba
Riccardo Camozzi <erdodopazzosgravo@gmail.com>
2024-01-08 15:47:38 +0100

    Aggiungere i file di progetto.

---
+++ Classe aliante - interfaccia/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+//classe componente come interfaccia
+public interface Componente
+{
+    string Descrizione();
+    double Costo();
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
