0cc7c4fc7a49be0dd2e4dfda53e48aa444de6b1b
Riccardo Camozzi <erdodopazzosgravo@gmail.com>
2024-01-08 15:56:40 +0100

    Aggiunta della classe aliante

--- Classe aliante - interfaccia/Program.cs
+++ Classe aliante - interfaccia/Program.cs
@@ -7,6 +7,51 @@ public interface Componente
     string Descrizione();
     double Costo();
 }
+
+public class Aliante : Componente
+{
+    private List<Componente> componenti = new List<Componente>();
+
+    public string Descrizione()
+    {
+        string descrizione = "Aliante:\n";
+
+        foreach (Componente componente in componenti)
+        {
+            descrizione += componente.Descrizione() + "\n";
+        }
+
+        return descrizione;
+    }
+
+    public double Costo()
+    {
+        double costoTotale = 0;
+
+        foreach (Componente componente in componenti)
+        {
+            costoTotale += componente.Costo();
+        }
+
+        return costoTotale;
+    }
+
+    public void Add(Componente componente)
+    {
+        componenti.Add(componente);
+    }
+
+    public void Remove(Componente componente)
+    {
+        componenti.Remove(componente);
+    }
+
+    public List<Componente> GetChild()
+    {
+        return componenti;
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
